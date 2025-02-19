99567d95e9235d768cffe70a2fec7b75219f30d9
Alkis <alkisax@gmail.com>
2025-02-19 19:44:08 +0200

    initial

--- index.js
+++ index.js
@@ -38,6 +38,11 @@ let notes = [
     id: "3",
     content: "GET and POST are the most important methods of HTTP protocol",
     important: true
+  },
+  {
+    id: "4",
+    content: "learn web services",
+    important: true
   }
 ]
 
