dfb46d5e199b05d7b69ea61d3e01a27bd3cd884a
Alkis <alkisax@gmail.com>
2025-03-26 17:36:40 +0200

    working or redux. separating redux files

--- frontend/src/reducers/noteReducer.jsx
+++ frontend/src/reducers/noteReducer.jsx
@@ -3,7 +3,7 @@ import { createStore } from 'redux'
 const noteReducer = (state = [], action) => {
   switch(action.type) {
     case 'NEW_NOTE':
-      return state.concat(action.payload)
+      return [...state, action.payload]
     case 'TOGGLE_IMPORTANCE': {
       const id = action.payload.id
       const noteToChange = state.find(n => n.id === id)
@@ -22,31 +22,62 @@ const noteReducer = (state = [], action) => {
 
 const store = createStore(noteReducer)
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-})
+// store.dispatch({
+//   type: 'NEW_NOTE',
+//   payload: {
+//     content: 'the app state is in redux store',
+//     important: true,
+//     id: 1
+//   }
+// })
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
+// store.dispatch({
+//   type: 'NEW_NOTE',
+//   payload: {
+//     content: 'state changes are made with actions',
+//     important: false,
+//     id: 2
+//   }
+// })
+
+const generateId = () =>
+  Number((Math.random() * 1000000).toFixed(0))
 
 // eslint-disable-next-line no-unused-vars, react-refresh/only-export-components
 const App = () => {
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    store.dispatch({
+      type: 'NEW_NOTE',
+      payload: {
+        content,
+        important: false,
+        id: generateId()
+      }
+    })
+  }
+
+  const toggleImportance = (id) => {
+    store.dispatch({
+      type: 'TOGGLE_IMPORTANCE',
+      payload: { id }
+    })
+  }
+
   return(
     <div>
+      <form onSubmit={addNote}>
+        <input name="note" /> 
+        <button type="submit">add</button>
+      </form>
       <ul>
         {store.getState().map(note=>
-          <li key={note.id}>
+          <li
+            key={note.id}
+            onClick={() => toggleImportance(note.id)}
+          >
             {note.content} <strong>{note.important ? 'important' : ''}</strong>
           </li>
         )}
